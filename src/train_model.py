b528db49011b115c5ca5d1fd35b572b4be8d448b
Sujoy Maji <sujoyphonee@gmail.com>
2026-04-17 09:15:00 +0530

    model: added ROC-AUC curve evaluation to assess churn prediction threshold

--- src/train_model.py
+++ src/train_model.py
@@ -1,3 +1,4 @@
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score, precision_score, recall_score
+from sklearn.metrics import roc_auc_score, roc_curve
